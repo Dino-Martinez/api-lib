07000027347be21ca4ca50be8009434be1a81d4f
Dino-Martinez <Dino.Martinez.514@gmail.com>
2022-02-10 23:42:45 -0500

    Exported function to its own file, and implemented callback

---
+++ index.js
@@ -0,0 +1,10 @@
+function getWeather(apiKey, zip, callback) {
+  const units = 'imperial'
+  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
+  fetch(path)
+    .then(res => res.json())
+    .then(json => {
+      callback(json)
+    })
+    .catch(err => console.log(err.message))
+}
