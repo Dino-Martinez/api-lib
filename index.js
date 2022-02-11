f82e3b93442bab6f4d5ce1c22cf97f88a0c57721
Dino-Martinez <Dino.Martinez.514@gmail.com>
2022-02-10 23:55:30 -0500

    Everything else

--- index.js
+++ index.js
@@ -1,7 +1,25 @@
-function getWeather(apiKey, zip) {
-  const units = 'imperial'
-  const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
-  return fetch(path)
-    .then(res => res.json())
-    .catch(err => console.log(err.message))
+const basePath = 'api.openweathermap.org/data/2.5/weather?'
+
+async function getWeather(apiKey, zip, units='imperial') {
+  const path = `${basePath}zip=${zip}&appid=${apiKey}&units=${units}`
+  const res = await fetch(path)
+  return await res.json()
+}
+
+async function getWeatherByCityName(apiKey, city, units='imperial') {
+  const path = `${basePath}q=${city}&appid=${apiKey}&units=${units}`
+  const res = await fetch(path)
+  return await res.json()
+}
+
+async function getWeatherByCityID(apiKey, city, units='imperial') {
+  const path = `${basePath}id=${city}&appid=${apiKey}&units=${units}`
+  const res = await fetch(path)
+  return await res.json()
+}
+
+async function getWeatherByGeo(apiKey, geo, units='imperial') {
+  const path = `${basePath}lat=${geo.lat}&lon=${geo.lon}&appid=${apiKey}&units=${units}`
+  const res = await fetch(path)
+  return await res.json()
 }
